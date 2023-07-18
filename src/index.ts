93a326a973be8ef9439a93c68b540ea711bb29fc
Marvin Ronquillo <marvin.r@lightweightsolutions.me>
2023-07-18 13:31:43 +0800

    fix:build

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(cors());
 
-app.use('/public', express.static(path.join(__dirname, 'public')));
+// app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.post('/webhook', (req, res) => {
   let body = req.body;
