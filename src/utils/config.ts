c292d4353ef12ce35505f9999e3a6dd2cd75b970
Marvin Ronquillo <marvin.r@lightweightsolutions.me>
2023-07-18 15:10:26 +0800

    fix:build

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 
 const config = {
-  PORT: process.env.APP_PORT,
+  PORT: process.env.PORT,
   FB_PAGE_ID: process.env.FB_PAGE_ID,
   FB_APP_ID: process.env.FB_APP_ID,
   FB_PAGE_ACCESS_TOKEN: process.env.FB_PAGE_ACCESS_TOKEN,
