6253c13aa4636bb3fbbdc65195009fdb3b625811
Marvin Ronquillo <marvin.r@lightweightsolutions.me>
2023-07-23 18:49:10 +0800

    refator:webhook

--- src/routes/webhookRouter.ts
+++ src/routes/webhookRouter.ts
@@ -93,22 +93,26 @@ webhookRouter.post('/', async (req, res) => {
                   recipient: {
                     id: psid,
                   },
-                  messaging_type: 'RESPONSE',
                   message: {
-                    text: 'Lightweight Solutions founded in 2008 is an expert in the field of IT and has worked with many multinational and leading enterprises in the country.\n\nThe company has developed innovations, software solutions and technology products in the areas of healthcare, finance, FMCG, real estate, education, entertainment and electrification among others.\n\nWe envision a world that is smarter, better and more enjoyable through simplified technology while making it sustainable for future generations as well. We believe in innovations and smart solutions that create valuable impact to improve lifestyle and businesses while making a better world.\n\nWe simplify technology for you. Let us help you reach your goals.\n\nLet us build a smart and sustainable world TOGETHER.',
-                    quick_replies: [
-                      {
-                        content_type: 'text',
-                        title: 'Learn More',
-                        payload: 'web_url',
-                        web_url: 'https://lightweightsolutions.co/',
+                    attachment: {
+                      type: 'template',
+                      payload: {
+                        template_type: 'button',
+                        text: `Lightweight Solutions founded in 2008 is an expert in the field of IT and has worked with many multinational and leading enterprises in the country.\n\nThe company has developed innovations, software solutions and technology products in the areas of healthcare, finance, FMCG, real estate, education, entertainment and electrification among others.\n\nWe envision a world that is smarter, better and more enjoyable through simplified technology while making it sustainable for future generations as well. We believe in innovations and smart solutions that create valuable impact to improve lifestyle and businesses while making a better world.\n\nWe simplify technology for you. Let us help you reach your goals.\n\nLet us build a smart and sustainable world TOGETHER`,
+                        buttons: [
+                          {
+                            type: 'web_url',
+                            title: 'Learn More',
+                            url: 'https://lightweightsolutions.co',
+                          },
+                          {
+                            type: 'postback',
+                            title: 'Go Back',
+                            payload: 'go_back',
+                          },
+                        ],
                       },
-                      {
-                        content_type: 'text',
-                        title: 'Go Back',
-                        payload: 'get_started',
-                      },
-                    ],
+                    },
                   },
                 },
                 {
