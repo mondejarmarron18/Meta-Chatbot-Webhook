0049f6db2c30fcf18f300eeb878e0e7037032c15
Marvin Ronquillo <marvin.r@lightweightsolutions.me>
2023-07-23 20:37:15 +0800

    refator:webhook

--- src/routes/webhookRouter.ts
+++ src/routes/webhookRouter.ts
@@ -48,7 +48,7 @@ webhookRouter.post('/', async (req, res) => {
 
         if (event?.message) {
           console.log(event.message);
-          postWelcome(psid);
+          return postWelcome(psid);
         } else if (event?.postback) {
           switch (event.postback?.payload) {
             case webhookPostbackPayload.goBack:
