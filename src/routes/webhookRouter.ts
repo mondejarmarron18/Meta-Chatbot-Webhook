c96bc0393c6bf1449b9bbf4631841aff6015c75c
Marvin Ronquillo <marvin.r@lightweightsolutions.me>
2023-07-23 18:07:42 +0800

    refator:webhook

--- src/routes/webhookRouter.ts
+++ src/routes/webhookRouter.ts
@@ -4,6 +4,35 @@ import api from '../utils/api';
 
 const webhookRouter = Router();
 
+webhookRouter.get('/', async (req, res) => {
+  const mode = req.query['hub.mode'];
+  const token = req.query['hub.verify_token'];
+  const challenge = req.query['hub.challenge'];
+
+  if (mode && token) {
+    if (mode !== 'subscribe' && token !== config.FB_VERIFY_TOKEN) {
+      res.sendStatus(403);
+    }
+
+    await api.post(
+      '/me/messenger_profile',
+      {
+        whitelisted_domains: ['https://lws-fb-chat-1c47aa033775.herokuapp.com'],
+        get_started: {
+          payload: 'greeting',
+        },
+      },
+      {
+        params: {
+          access_token: config.FB_PAGE_ACCESS_TOKEN,
+        },
+      }
+    );
+
+    res.status(200).send(challenge);
+  }
+});
+
 webhookRouter.post('/', async (req, res) => {
   const body = req.body;
 
@@ -12,9 +41,7 @@ webhookRouter.post('/', async (req, res) => {
       entry.messaging.forEach(async (event: any) => {
         const psid = event.sender.id;
 
-        console.log(event.sender);
-
-        api.post(
+        await api.post(
           `/me/messages`,
           {
             recipient: {
@@ -25,7 +52,7 @@ webhookRouter.post('/', async (req, res) => {
                 type: 'template',
                 payload: {
                   template_type: 'button',
-                  text: `Hi {{user_first_name}}, Welcome to Lightweight Solutions Page! 😊 Please choose from the options below to learn more.`,
+                  text: `Hi ${event?.postback}, Welcome to Lightweight Solutions Page! 😊 Please choose from the options below to learn more.`,
                   buttons: [
                     {
                       type: 'postback',
@@ -59,20 +86,6 @@ webhookRouter.post('/', async (req, res) => {
     res.status(200).send('EVENT_RECEIVED');
   } else {
     res.sendStatus(404);
-  }
-});
-
-webhookRouter.get('/', async (req, res) => {
-  const mode = req.query['hub.mode'];
-  const token = req.query['hub.verify_token'];
-  const challenge = req.query['hub.challenge'];
-
-  if (mode && token) {
-    if (mode === 'subscribe' && token === config.FB_VERIFY_TOKEN) {
-      return res.send(challenge);
-    }
-
-    res.sendStatus(403);
   }
 });
 
