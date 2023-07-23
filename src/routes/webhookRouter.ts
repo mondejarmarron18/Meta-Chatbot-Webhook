700e6d07e76609c6e9296747975d7af75b1929c5
Marvin Ronquillo <marvin.r@lightweightsolutions.me>
2023-07-23 17:02:59 +0800

    refactor:webhook

--- src/routes/webhookRouter.ts
+++ src/routes/webhookRouter.ts
@@ -8,7 +8,14 @@ webhookRouter.post('/', async (req, res) => {
   const body = req.body;
 
   if (body.object === 'page') {
-    console.log(body.entry);
+    body.entry.forEach((entry: any) => {
+      entry.messaging.forEach(async (event: any) => {
+        const psid = event.sender.id;
+
+        console.log(event);
+      });
+    });
+
     res.status(200).send('EVENT_RECEIVED');
   } else {
     res.sendStatus(404);
@@ -19,54 +26,13 @@ webhookRouter.get('/', async (req, res) => {
   const mode = req.query['hub.mode'];
   const token = req.query['hub.verify_token'];
   const challenge = req.query['hub.challenge'];
-  const entry = req.body.entry;
 
   if (mode && token) {
     if (mode !== 'subscribe' && token !== config.FB_VERIFY_TOKEN) {
       res.sendStatus(403);
     }
 
-    console.log(entry);
-
-    try {
-      await api.post(`/me/messenger_profile`, {
-        params: {
-          access_token: config.FB_PAGE_ACCESS_TOKEN,
-        },
-        data: {
-          get_started: {
-            payload: 'greeting',
-          },
-          persistent_menu: [
-            {
-              locale: 'default',
-              composer_input_disabled: false,
-              call_to_actions: [
-                {
-                  type: 'postback',
-                  title: 'Restart Bot',
-                  payload: 'restart',
-                },
-                {
-                  type: 'postback',
-                  title: 'Read Full Mechanics',
-                  payload: 'mechanics',
-                },
-                {
-                  type: 'postback',
-                  title: 'Send an Inquiry',
-                  payload: 'inquiries',
-                },
-              ],
-            },
-          ],
-        },
-      });
-
-      res.send(challenge);
-    } catch (error) {
-      console.log(error);
-    }
+    res.send(challenge);
   }
 });
 
