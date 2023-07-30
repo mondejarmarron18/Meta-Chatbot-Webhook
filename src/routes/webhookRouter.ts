41efd635c3178134cf95e5f0ffda3e3273b2b531
Marvin Ronquillo <marvin.r@lightweightsolutions.me>
2023-07-30 21:31:20 +0800

    feature:get started button

--- src/routes/webhookRouter.ts
+++ src/routes/webhookRouter.ts
@@ -24,66 +24,25 @@ webhookRouter.get("/", async (req, res) => {
       res.sendStatus(403);
     }
 
-    // await api.post(
-    //   "/me/messenger_profile",
-    //   {
-    //     get_started: { payload: "get_started" },
-    //     greeting: [
-    //       {
-    //         locale: "default",
-    //         text: "Hello {{user_first_name}}!",
-    //       },
-    //     ],
-    //   },
-    //   {
-    //     params: {
-    //       access_token: config.FB_PAGE_ACCESS_TOKEN,
-    //     },
-    //   }
-    // );
-
-    // res.status(200).send(challenge);
-    await fetch(
-      `https://graph.facebook.com/v17.0/me/messenger_profile?access_token=${config.FB_PAGE_ACCESS_TOKEN}`,
+    await api.post(
+      "/me/messenger_profile",
       {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          whitelisted_domains: [config.FB_WEBVIEW_URL],
-          get_started: {
-            payload: "greeting",
+        get_started: { payload: "get_started" },
+        greeting: [
+          {
+            locale: "default",
+            text: "Hello {{user_first_name}}!",
           },
-          persistent_menu: [
-            {
-              locale: "default",
-              composer_input_disabled: false,
-              call_to_actions: [
-                {
-                  type: "postback",
-                  title: "Restart Bot",
-                  payload: "restart",
-                },
-                {
-                  type: "postback",
-                  title: "Read Full Mechanics",
-                  payload: "mechanics",
-                },
-                {
-                  type: "postback",
-                  title: "Send an Inquiry",
-                  payload: "inquiries",
-                },
-              ],
-            },
-          ],
-        }),
+        ],
+      },
+      {
+        params: {
+          access_token: config.FB_PAGE_ACCESS_TOKEN,
+        },
       }
-    ).then(() => {
-      console.log("WEBHOOK_VERIFIED");
-      res.send(challenge);
-    });
+    );
+
+    res.status(200).send(challenge);
   }
 });
 
